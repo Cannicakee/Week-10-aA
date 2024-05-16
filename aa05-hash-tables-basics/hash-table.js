6e016fe25e3dc1fc5fead45c2fc0019d096af162
Cannicakee <stanfill11@gmail.com>
2024-05-15 22:09:12 -0600

    solved hash tables

--- aa05-hash-tables-basics/hash-table.js
+++ aa05-hash-tables-basics/hash-table.js
@@ -11,27 +11,65 @@ class KeyValuePair {
 class HashTable {
 
   constructor(numBuckets = 4) {
-    // Your code here 
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null)
+    this.count = 0;
   }
 
   hash(key) {
-    // Your code here 
+    let hashKey = sha256(key);
+    return parseInt(hashKey.substring(0, 8), 16)
   }
 
   hashMod(key) {
-    // Your code here 
+    let val = this.hash(key);
+    return val % this.capacity; ``
   }
 
   insertNoCollisions(key, value) {
-    // Your code here 
+    let hashedIndex = this.hashMod(key)
+    let dataCheck = this.data[hashedIndex]
+    if (dataCheck !== null) {
+      throw new Error("hash collision or same key/value pair already exists!")
+    }
+
+    this.data[hashedIndex] = new KeyValuePair(key, value)
+    this.count++
   }
 
   insertWithHashCollisions(key, value) {
-    // Your code here 
+    let hashedIndex = this.hashMod(key)
+    let newKV = new KeyValuePair(key, value);
+    if (this.data[hashedIndex] === null) {
+      this.data[hashedIndex] = newKV
+    } else {
+      newKV.next = this.data[hashedIndex]
+      this.data[hashedIndex] = newKV;
+    }
+    this.count++
   }
 
   insert(key, value) {
-    // Your code here 
+    let hashedIndex = this.hashMod(key);
+    let existingPair = this.data[hashedIndex]
+    let newKV = new KeyValuePair(key, value)
+
+    if (existingPair === null) {
+      this.data[hashedIndex] = newKV
+    } else {
+      let current = existingPair;
+      while (current) {
+        if (current.key === key) {
+          current.value = value;
+          return;
+        }
+        if (!current.next) break;
+        current = current.next;
+      }
+      newKV.next = existingPair;
+      this.data[hashedIndex] = newKV
+    }
+    this.count++;
   }
 
 }
